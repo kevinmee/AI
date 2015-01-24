ba12b922f2eae58f1662da24515156e63249656a
kevinmee <kevin.mee@gmail.com>
2015-01-24 11:50:46 -0500

    copied over the player info from the test player

--- ConnectN/src/ConnectN_player.java
+++ ConnectN/src/ConnectN_player.java
@@ -1,4 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
+
+import referee.TestPlayer;
+
 
 public class ConnectN_player {
 
+	String playerName="This is our player name!";
+	BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+	boolean first_move=false;
+	
+	public void processInput() throws IOException{	
+	
+    	String s=input.readLine();	
+		List<String> ls=Arrays.asList(s.split(" "));
+		if(ls.size()==2){
+			System.out.println(ls.get(0)+" "+ls.get(1));
+		}
+		else if(ls.size()==1){
+			System.out.println("game over!!!");
+			System.exit(0);
+		}
+		else if(ls.size()==5){          //ls contains game info
+			System.out.println("0 1");  //first move
+		}
+		else if(ls.size()==4){		//player1: aa player2: bb
+			//TODO combine this information with game information to decide who is the first player
+		}
+		else
+			System.out.println("not what I want");
+	}
+	
+	public static void main(String[] args) throws IOException {
+		ConnectN_player rp=new ConnectN_player();
+		System.out.println(rp.playerName);
+		while (true){
+			rp.processInput();
+		}
+
+	}
+
+	
+	
 }
