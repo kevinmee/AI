8859277ee45101246d0a78b08ce1c3ebf58ca2b5
kevinmee <kevin.mee@gmail.com>
2015-01-25 17:41:20 -0500

    added the initial minimax to the player

--- ConnectN/src/player/ConnectNNode.java
+++ ConnectN/src/player/ConnectNNode.java
@@ -22,44 +22,6 @@ public class ConnectNNode {
 	public List<ConnectNNode> getChildren(){
 		return children;
 	}
-
-	/*
-	 * ***********************************************
-	 * ---- To be placed in the player file! -------
-	 * ***********************************************
-	 */
-	private int miniMax(ConnectNNode currentNode, int depth, int alpha, int beta) {
-		if (depth <= 0 ) {
-			return getHeuristic(currentNode.getState());
-		}
-
-		// 0 is for our own player
-		if (currentNode.getState().player == 0) {
-			for (ConnectNNode child : currentNode.getChildren()) {
-				alpha = Math.max(alpha, miniMax(child, depth - 1, alpha, beta));
-
-				// this needs tweaking
-				if (alpha >= beta) {
-					return beta;
-				}
-			}
-			return alpha;
-		} else {
-			for (ConnectNNode child : currentNode.getChildren()) {
-				beta = Math.min(beta, miniMax(child, depth - 1, alpha, beta));
-
-				// needs some tweaking
-				if (alpha >= beta) {
-					return alpha;
-				}
-			}
-			return beta;
-
-		}
-	}
-	
-
-	
 	
 	
 	
--- ConnectN/src/player/ConnectN_player.java
+++ ConnectN/src/player/ConnectN_player.java
@@ -61,6 +61,41 @@ public class ConnectN_player {
 		}
 
 	}
+	
+	private int miniMax(ConnectNNode currentNode, int depth, int alpha, int beta) {
+		if (depth <= 0 ) {
+			return getHeuristic(currentNode.getState());
+		}
+
+		// 0 is for our own player
+		if (currentNode.getState().player == 0) {
+			for (ConnectNNode child : currentNode.getChildren()) {
+				alpha = Math.max(alpha, miniMax(child, depth - 1, alpha, beta));
+
+				// this needs tweaking
+				if (alpha >= beta) {
+					return beta;
+				}
+			}
+			return alpha;
+		} else {
+			for (ConnectNNode child : currentNode.getChildren()) {
+				beta = Math.min(beta, miniMax(child, depth - 1, alpha, beta));
+
+				// needs some tweaking
+				if (alpha >= beta) {
+					return alpha;
+				}
+			}
+			return beta;
+
+		}
+	}
+
+	private int getHeuristic(ConnectNNode state) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 	
 	
